e7861f0e710e2a7397cf02efb1ba1df97d8fc308
PradeepS081 <pradeeps@nousinfo.com>
2024-09-13 14:47:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const core = require('@actions/core');
 
-try {
-  const name = core.getInput('name');
-  console.log(`Hello ${name}!`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+        const name = core.getInput('name');
+        console.log(`Hello, ${name}!`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
